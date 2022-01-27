0f5eee5c867c47d6368fecc280187bf95a77d69a
Kazen Gallman <kazar444@icloud.com>
2022-01-27 16:35:19 -0500

    Updating command needed to cancel morning meow

--- morningmeow/fileManage2.py
+++ morningmeow/fileManage2.py
@@ -35,7 +35,7 @@ stripe.api_key = readAuthFiles("./authFiles.txt")["stripe_secret_key"]
 
 
 welcome_text = "Hello {}, and welcome to the offical launch of " \
-                "MorningMeow! respond with STOP to cancel. \n\nIf you find a bug or issue " \
+                "MorningMeow! respond with \"STOP.\" to cancel. \n\nIf you find a bug or issue " \
                 'it would help us a lot if you could send a text back as follows \n"BUG (issue here without parentheses)"' \
                 "\n\nHere is a message from the person who signed you up:\n\n{}"
 
